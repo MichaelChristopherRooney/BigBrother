0ef3f2a7ed66dbe53fcd8b71736bd225215600a4
Michael Rooney <m.rooney93@gmail.com>
2015-09-21 20:52:27 +0100

    Update sv_init.lua

--- lua/autorun/server/sv_init.lua
+++ lua/autorun/server/sv_init.lua
@@ -2,16 +2,16 @@
 	authorisation_type is how we chose who can view events
 	"ulx" uses ulx ranks, "fadmin" uses fadmin ranks, and "specified" uses a list of specified SteamIDs
 ]]
-local authorisation_type = "ulx"
-local authorised_users = {"admin", "superadmin"}
+authorisation_type = "ulx"
+authorised_users = {"admin", "superadmin"}
 
 --[[
 	values relating to how often data is recorded and what data is recorded
 ]]
-local record_locations_frequency = 5 -- how often location should be record in seconds
-local event_lifespan = 1800 -- how long events should be stored for in seconds (600 = 10 min, 1800 = 30 min, etc)
+record_locations_frequency = 5 -- how often location should be record in seconds
+event_lifespan = 1800 -- how long events should be stored for in seconds (600 = 10 min, 1800 = 30 min, etc)
 
-local events = {}
+events = {}
 
 
 -- initialise the addon
@@ -19,22 +19,45 @@ function InitialiseBigBrother()
 
 	if not verifyConfig() then return end
 	
-	timer.Create("record_locations_timer", 5, 0, recordLocations) 
+	timer.Create("record_locations_timer", record_locations_frequency, 0, recordLocations) 
 
 end
-hook.Add("Initialise", "InitialiseBB", InitialiseBigBrother )
+hook.Add("Initialize", "InitialiseBB", InitialiseBigBrother )
+
 
 -- ensure all config values are well formed
 function verifyConfig()
 
-	if not authorisation_type == "ulx" and not authorisation_type == "fadmin" and not authorisation_type == "specified" then
+	if (authorisation_type ~= "ulx") and (authorisation_type ~= "fadmin") and (authorisation_type ~= "specified") then
 		print("*** BigBrother error: authorisation_type is not a recognised value ***")
 		return false
 	end
 	
+	if type(record_locations_frequency) ~= "number" then
+		print("*** BigBrother error: record_locations_frequency must be a number ***")
+		return false
+	end
+	
+	if type(event_lifespan) ~= "number" then
+		print("*** BigBrother error: event_lifespan must be a number ***")
+		return false
+	end
+	
+	print("BigBrother log: Initialization ok")
+	print("BigBrother log: Authorisation type is " .. authorisation_type)
+	print("BigBrother log: Authorised users are: ")
+	
+	for k, v in pairs( authorised_users ) do
+		print(k, v)
+	end
+	
+	return true
+
 end
 
 
+-- record the location of every player
+-- called at a given interval by a "record_locations_timer"
 function recordLocations()
 
 	plys = player.GetAll()
@@ -53,32 +76,17 @@ function recordLocations()
 
 end
 
+
 -- need to rework this into a command to open a gui clientside
-function donateCommand( pl, text, teamonly )
-            if (text == "!locations") then
-                for k, v in pairs(player.GetAll()) do v:ChatPrint( "Player " .. pl:Nick() .. " has donated to the server via !donate" )
+function display_events( pl, text, teamonly )
 
-        end
-    end
-end
-hook.Add( "PlayerSay", "Chat", donateCommand )
-
-
---[[
-
-	plys = player.GetAll()
+	if (text == "!locations") then
 	
-	for i = 1, #plys, 1 do
-		print(plys[i]:SteamID())
-		print(plys[i]:GetPos()[0])
-		print(plys[i]:GetPos())
-    end
-	
-	for n = 1, #events, 1 do
-		print(events[n].player_steam_id)
-		print(events[n].event_type)
-		print(events[n].timestamp)
-		print(events[n].position)
+		for k, v in pairs(events[1]) do
+			print(k, v)
+		end
+
 	end
 	
-]]
+end
+hook.Add( "PlayerSay", "Chat", display_events )
