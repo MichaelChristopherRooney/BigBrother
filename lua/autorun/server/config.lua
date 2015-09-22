71dc2b48c24e7352d2fbe118f244840280551486
Michael Rooney <m.rooney93@gmail.com>
2015-09-22 19:49:34 +0100

    Change tab width

--- lua/autorun/server/config.lua
+++ lua/autorun/server/config.lua
@@ -14,15 +14,15 @@ delete_events_frequency = 5 -- how often the addon should check for old events t
 -- what events should be recorded
 -- change True to False if you do not want that event to be recorded
 events_to_record = {
-	["spawned_prop"] = True,			-- the player spawned a prop
-	["spawned_sent"] = True,			-- the player spawned a scripted entity
-	["spawned_swep"] = True,			-- the player spawned a scripted weapon
-	["was_killed"] = True,				-- the player was killed_player
-	["suicide"] = True,					-- the player committed suicide
-	["killed_player"] = True,			-- the player killed another player
-	["connected"] = True,				-- the player connected to the game
-	["disconnected"] = True,			-- the player disconnected from the game
-	["say"] = True,						-- the player said something in chat
-	["changed_team_or_job"] = True,		-- the player changed their team or job
-	["spawned"] = True					-- the player spawned
+	["spawned_prop"] = True,	-- the player spawned a prop
+	["spawned_sent"] = True,	-- the player spawned a scripted entity
+	["spawned_swep"] = True,	-- the player spawned a scripted weapon
+	["was_killed"] = True,		-- the player was killed_player
+	["suicide"] = True,		-- the player committed suicide
+	["killed_player"] = True,	-- the player killed another player
+	["connected"] = True,		-- the player connected to the game
+	["disconnected"] = True,	-- the player disconnected from the game
+	["say"] = True,			-- the player said something in chat
+	["changed_team_or_job"] = True,	-- the player changed their team or job
+	["spawned"] = True		-- the player spawned
 }
--- lua/autorun/server/event_functions.lua
+++ lua/autorun/server/event_functions.lua
@@ -1,6 +1,7 @@
 -- this file contains functions that:
---		deletes old events
---		pulls data in response to client queries
+--	deletes old events
+--	pulls data in response to client queries
+
 
 -- this function checks if any events are old enough to be deleted
 function deleteEvents()
